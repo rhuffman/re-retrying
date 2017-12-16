5bbd2961b00624de73fa46c8b49a279e2ca9d7dc
Robert Huffman <robert.huffman@gmail.com>
2017-12-16 11:20:44 -0800

    Fix copyright notices

--- src/main/java/com/github/rholder/retry/AttemptTimeLimiter.java
+++ src/main/java/com/github/rholder/retry/AttemptTimeLimiter.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2012-2015 Ray Holder
+ * Modifications copyright 2017 Robert Huffman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
